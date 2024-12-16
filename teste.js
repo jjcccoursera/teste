f63cb8a3bc90cf0ef93fa8585d42229190023f06
jjcccoursera <jcarmona.coursera@gmail.com>
2024-12-16 11:07:00 +0000

    Inicial

---
+++ teste.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Test app running on port 8080');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
